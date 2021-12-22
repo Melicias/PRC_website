34111b4da1449571e047ecbd54bd6dac63b28620
Melicias <melicias1999@gmail.com>
2021-12-22 16:04:58 +0000

    alterei alguma cena e passou a dar nice

--- prc/src/main/java/com/example/prc/dtos/ProfissionaSaudeDTO.java
+++ prc/src/main/java/com/example/prc/dtos/ProfissionaSaudeDTO.java
@@ -1,4 +1,16 @@
 package com.example.prc.dtos;
 
+import com.example.prc.entities.TipoProfissional;
+import com.example.prc.entities.Utente;
+
+import java.util.List;
+
 public class ProfissionaSaudeDTO {
+    private TipoProfissional tipoProfissional;
+    private List<Utente> utentes;
+
+    public ProfissionaSaudeDTO(TipoProfissional tipoProfissional, List<Utente> utentes) {
+        this.tipoProfissional = tipoProfissional;
+        this.utentes = utentes;
+    }
 }
